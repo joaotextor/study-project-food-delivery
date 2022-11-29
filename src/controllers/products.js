e3313f97b9463e8e1a3ea99da52d42d5ddc2e861
Joao Textor <joaotextor@gmail.com>
2022-11-29 14:27:33 -0300

    products post and delete

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -8,7 +8,41 @@ const get = async (req, res) => {
     res.send(products)
 }
 
+const post = async (req, res) => {
+    const {
+        name,
+        price
+    } = req.body
+
+    const products = new ProductsModels({
+        name,
+        price
+    })
+
+    products.save()
+
+    res.send({
+        message: 'Product successfully inserted'
+    })
+}
+
+const remove = async (req, res) => {
+    const { id } = req.params
+
+    const remove = await ProductsModels.deleteOne({ _id: id})
+
+    const message = remove.deletedCount === 1 ? 'successfully deleted' : 'error deleting product'
+
+    res.send({
+        message
+    })
+
+}
+
+
 
 module.exports = {
     get,
+    post,
+    remove
 }
