84de0bb083678fd25c650726971e089a7c02e9f7
Joao Textor <joaotextor@gmail.com>
2022-11-29 11:27:59 -0300

    created products Model

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,8 +4,12 @@ const router = require('express').Router()
 // const OrdersControlller = require('../controllers/orders')
 const CustomersController = require('../controllers/customers')
 
+//Customers
 router.get('/customers/:id?', CustomersController.get)
 router.post('/customers', CustomersController.post)
 router.delete('/customers/:id', CustomersController.remove)
+
+//Products
+
 
 module.exports = router
