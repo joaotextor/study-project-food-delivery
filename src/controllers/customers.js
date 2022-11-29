186722cc80ad050313d99548f9ee935c866fe8f1
Joao Textor <joaotextor@gmail.com>
2022-11-29 11:10:45 -0300

    Customers: get, post, delete

---
+++ src/controllers/customers.js
@@ -0,0 +1,48 @@
+const CustomersModels = require('../modules/customers')
+
+const get = async (req, res) => {
+    const { id } = req.params
+    const customers = await CustomersModels.find(id ? { _id: id } : null)
+
+    res.send(customers)
+}
+
+const post = async (req, res) => {
+    const {
+        name,
+        email,
+        phone,
+        address
+    } = req.body
+
+    const customers = new CustomersModels({
+        name,
+        email,
+        phone,
+        address
+    })
+
+    customers.save()
+
+    res.send({
+        message: 'Customer successfully inserted'
+    })
+}
+
+const remove = async (req, res) => {
+    const { id } = req.params
+
+    const remove = await CustomersModels.deleteOne({ _id: id})
+
+    const message = remove.deletedCount === 1 ? 'success' : 'error'
+
+    res.send({
+        message
+    })
+}
+
+module.exports = {
+    get,
+    post,
+    remove
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 
-// const ProductsController = require('./controllers/products')
-// const OrdersControlller = require('./controllers/orders')
-// const CustomersController = require('./controllers/customers')
+// const ProductsController = require('../controllers/products')
+// const OrdersControlller = require('../controllers/orders')
+const CustomersController = require('../controllers/customers')
 
-
+router.get('/customers/:id?', CustomersController.get)
+router.post('/customers', CustomersController.post)
+router.delete('/customers/:id', CustomersController.remove)
 
 module.exports = router
