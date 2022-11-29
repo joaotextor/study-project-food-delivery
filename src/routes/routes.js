9eaee0e8b03a5a38e13feed8d5174c4df750834b
Joao Textor <joaotextor@gmail.com>
2022-11-29 10:05:43 -0300

    drafting routes.js and changed file names

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 
+// const ProductsController = require('./controllers/products')
+// const OrdersControlller = require('./controllers/orders')
+// const CustomersController = require('./controllers/customers')
+
 
 
 module.exports = router
