c605f38e78c541edeeca79305c7423e4629314f1
Joao Textor <joaotextor@gmail.com>
2022-12-06 11:41:40 -0300

    customers page initial html draft

---
+++ front/admin/js/customers.js
@@ -0,0 +1,26 @@
+import { API_URL } from 'utils.js'
+
+const Customers = {
+    init: function() {
+        this.cacheElements()
+        this.bindEvents()
+    },
+
+    cacheElements: function() {
+
+    },
+
+    bindEvents: function() {
+        const self = this
+    },
+
+    get: function() {
+        
+    },
+
+    Events: {
+
+    }
+}
+
+Customers.init()
